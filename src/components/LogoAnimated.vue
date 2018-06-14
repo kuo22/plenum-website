3138175f3fd0e180ff9a79fa3a6fa77d8cad125b
Jackson Ryan <jackson.baker.ryan@gmail.com>
2018-06-13 18:38:16 -0700

    Randomize start of animated logo

--- src/components/LogoAnimated.vue
+++ src/components/LogoAnimated.vue
@@ -19,47 +19,48 @@ import p5 from 'p5';
 // Submenu associated with a unique main menu entry
 export default class LogoAnimated extends Vue {
     private p5: p5;
-    @Prop() private play: boolean = true;
-    private frame: number = 0;
+    @Prop() private play: boolean = false;
+    private frame: number = Math.floor(Math.random() * 200) + 1;
 
     constructor() {
         super();
     }
 
+    // When this component is loaded, draw a canvas with an animatable Plenum logo
     public created() {
         const sketch = (p) => {
             const canvasWidth = 240;
             const canvasHeight = 240;
 
-            // p.preload = () => {
-            //
-            // };
-
+            // p5 canvas setup
             p.setup = () => {
                 p.createCanvas(canvasWidth, canvasHeight);
             };
 
+            // p5 drawing loop
             p.draw = () => {
                 p.background(255);
                 p.noFill();
 
-                const frameCount = this.frame;
+                const animPos = this.frame; // linear position of the animation
 
-                const rotateSquare = false;
+                const rotateSquare = false; // whether to rotate the thick square
 
                 for (let i = 0; i < 4; i++) {
                     p.push();
                     p.translate(p.width / 2, p.height / 2);
 
+
                     if (i === 0 && rotateSquare !== true) {
                         p.rotate(0);
                     } else if (i % 2 === 0) {
-                        p.rotate(-frameCount / (i * 50.0 + 100.0));
+                        p.rotate(-animPos / (i * 50.0 + 100.0));
                     } else {
-                        p.rotate(frameCount / (i * 50.0 + 100.0));
+                        p.rotate(animPos / (i * 50.0 + 100.0));
                     }
 
-                    polygon(0, 0, 100 - i * 5.0, i + 4);  // Square
+
+                    polygon(0, 0, 100 - i * 5.0, i + 4);
                     p.pop();
                 }
 
@@ -67,6 +68,12 @@ export default class LogoAnimated extends Vue {
                     this.frame++;
                 }
 
+                // Returns a regular polygon of a specific size, location, and specific number of points
+                // parameter(s) needed:
+                //      x       = x-coordinate
+                //      y       = y-coordinate
+                //      radius  = radius of a polygon's circumcircle
+                //      npoints = number of points on the polygon
                 function polygon(x, y, radius, npoints) {
                     const angle = p.TWO_PI / npoints;
                     p.beginShape();
